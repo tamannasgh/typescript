d1dc8aff7d6279fc4611053f8cb1117f89d39b5b
Tamanna Sharma <ts.harma2882006@gmail.com>
2024-11-05 15:18:13 +0530

    added more types

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ var user = { name: "tamanna", age: 18 };
 //we are telling that this user us a object that have 2 properties name which is of type string and age of type number
 //tuples are just like array just that we know exact number of elements and type of them (usestate know exactly that it will have 2 values first the type given when calling and seconnd is setter function agr smjh aaya to thik nhi to koi ni bss dimag mei aaya to likh diya)
 var tup = ["tamanna", 18]; //tis is a tuple that has only 2 elements first is of type string and second of type number
+//its full of error that saying something like block scope we will come to thiss soonnn
+var badPractice; //any let badPractice:any;
+badPractice = 5;
+badPractice = "hehe";
+//we are again writing the js means we are cutting off typescripts al super powers so we should never use it
+function print() {
+    console.log("hey wecome!");
+    // retrun 1; //error as i have said that the return type will be void
+}
+var welcome = print(); //if i hover to the welcome variable its showing the type void.
+var statusCodes;
+(function (statusCodes) {
+    statusCodes[statusCodes["notFound"] = 404] = "notFound";
+    statusCodes[statusCodes["success"] = 200] = "success";
+})(statusCodes || (statusCodes = {})); //
+console.log(statusCodes.success);
+//what are enums are written in notes at https://ribbon-buffalo-067.notion.site/Typescript-134fee81f6468004ab0ec039783e73d0?pvs=4
